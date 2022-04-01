e82b9fe47f0cfbffd5d357f2ac230e678ee944c9
Dmytrii Babiuk <airixonmlg@gmail.com>
2022-04-01 11:58:56 +0300

    barbershop done

---
+++ js/header.js
@@ -0,0 +1,8 @@
+
+$(document).on('scroll', function(){
+    if ( $(window).scrollTop() > 50) {
+        $('.header').addClass('scrolled');
+    } else {
+        $('.header').removeClass('scrolled');
+    }
+});
